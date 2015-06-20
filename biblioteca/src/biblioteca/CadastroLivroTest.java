59be9a432551193c6dd46e91cebdca18e29c2cb9
dsmoura <dsmoura@inf.ufrgs.br>
2015-06-20 09:12:55 -0300

    Refatoração dos testes.

--- biblioteca/src/biblioteca/CadastroLivroTest.java
+++ biblioteca/src/biblioteca/CadastroLivroTest.java
@@ -12,7 +12,7 @@ import excecoes.LivroVazioException;
 
 public class CadastroLivroTest {
 	
-	private Acervo acervo = new Acervo();
+	private Acervo acervo;
 	
 	@Before
 	public void preparaAcervo() {
@@ -20,90 +20,72 @@ public class CadastroLivroTest {
 	}
 
 	@Test
-	public void verificarAcervoVazio() {
+	public void deveConterAcervoVazio() {
 		assertEquals(0, acervo.getQuantidadeLivros());
 	}
 	
 	@Test
-	public void cadastraUmLivro() {
-		Livro livro = new Livro("8172631208973", "Clean Code");
-		cadastrarLivro(livro);
+	public void deveCadastrarUmLivro() {
+		cadastrarLivro("8172631208973", "Clean Code");
 		assertEquals(1, acervo.getQuantidadeLivros());
 	}
 	
 	@Test
-	public void cadastraDoisLivros() {
-		Livro livro = new Livro("8172631208973", "Clean Code");
-		cadastrarLivro(livro);
-		livro = new Livro("2983741234234", "JavaEE for Dummies");
-		cadastrarLivro(livro);
+	public void deveCadastrarDoisLivros() {
+		cadastrarLivro("8172631208973", "Clean Code");
+		cadastrarLivro("2983741234234", "JavaEE for Dummies");
 		assertEquals(2, acervo.getQuantidadeLivros());
 	}
 
-	private void cadastrarLivro(Livro livro) {
-		acervo.cadastrar(livro);
-	}
-	
 	@Test(expected=LivroVazioException.class)
-	public void cadastraLivroVazio() {
+	public void deveImpedirCadastrarLivroVazio() {
 		acervo.cadastrar(null);
 	}
 	
 	@Test
-	public void pesquisarLivroNaoExistente() {
-		List<Livro> retornoDePesquisa = acervo.pesquisar("");
-		assertEquals(retornoDePesquisa.size(), 0);
+	public void deveEncontrarNenhumLivroNoAcervo() {
+		List<Livro> livrosEncontrados = acervo.pesquisar("");
+		assertEquals(livrosEncontrados.size(), 0);
 	}
 	
 	@Test
-	public void pesquisarLivroExistentePorTitulo() {
-		Livro livro = new Livro("12345", "TDD By Example");
-		cadastrarLivro(livro);
-		List<Livro> retornoDePesquisa = acervo.pesquisar("TDD By Example");
-		assertEquals(1, retornoDePesquisa.size());
-	}
-	
-	@Test
-	public void pesquisarLivroEmAcervoComTresLivros() {
-		Livro livro = new Livro("12345", "TDD By Example");
-		cadastrarLivro(livro);
-		livro = new Livro("123456", "Harry Potter");
-		cadastrarLivro(livro);
-		livro = new Livro("1234567", "Senhor dos Anéis");
-		cadastrarLivro(livro);
+	public void deveEncontrarLivroNoAcervo() {
+		cadastrarLivro("12345", "TDD By Example");
+		cadastrarLivro("123456", "Harry Potter");
+		cadastrarLivro("1234567", "Senhor dos Anéis");
 		
-		List<Livro> retornoDePesquisa = acervo.pesquisar("TDD By Example");
-		assertEquals(1, retornoDePesquisa.size());
-		assertEquals("TDD By Example", retornoDePesquisa.get(0).getTitulo());
+		List<Livro> livrosEncontrados = acervo.pesquisar("TDD By Example");
+		assertEquals(1, livrosEncontrados.size());
+		assertEquals("TDD By Example", livrosEncontrados.get(0).getTitulo());
 	}
 	
 	@Test
-	public void pesquisarLivrosPorTrechoDoTituloEmAcervoComTresLivros() {
-		Livro livro = new Livro("12345", "TDD By Example");
-		cadastrarLivro(livro);
-		livro = new Livro("123456", "TDD na pratica");
-		cadastrarLivro(livro);
-		livro = new Livro("1234567", "Senhor dos Anéis");
-		cadastrarLivro(livro);
+	public void deveEncontrarLivrosNoAcertoPorTrechoDoTitulo() {
+		cadastrarLivro("12345", "TDD By Example");
+		cadastrarLivro("123456", "TDD na pratica");
+		cadastrarLivro("1234567", "Senhor dos Anéis");
 		
-		List<Livro> retornoDePesquisa = acervo.pesquisar("TDD");
-		assertEquals(2, retornoDePesquisa.size());
-		assertEquals("TDD By Example", retornoDePesquisa.get(0).getTitulo());
-		assertEquals("TDD na pratica", retornoDePesquisa.get(1).getTitulo());
+		List<Livro> livrosEncontrados = acervo.pesquisar("TDD");
+		assertEquals(2, livrosEncontrados.size());
+		assertEquals("TDD By Example", livrosEncontrados.get(0).getTitulo());
+		assertEquals("TDD na pratica", livrosEncontrados.get(1).getTitulo());
 	}
 	
 	@Test
-	public void consultarLivroExistentePorISBN() throws LivroNaoExistenteException {
-		Livro livroCleanCode = new Livro("283764", "Clean Code");
-		cadastrarLivro(livroCleanCode);
-		Livro livroTDD = new Livro("12345", "TDD By Example");
-		cadastrarLivro(livroTDD);
-		Livro retornoDePesquisa = acervo.consultarPorISBN("12345");
-		assertEquals(livroTDD, retornoDePesquisa);
+	public void deveConsultarLivroPorISBN() throws LivroNaoExistenteException {
+		cadastrarLivro("283764", "Clean Code");
+		cadastrarLivro("12345", "TDD By Example");
+		Livro livroEncontrado = acervo.consultarPorISBN("12345");
+		assertEquals("TDD By Example", livroEncontrado.getTitulo());
 	}
 		
 	@Test(expected = LivroNaoExistenteException.class)
-	public void consultarLivroNaoExistentePorISBN() throws LivroNaoExistenteException{
+	public void deveRetornarErroAoConsultarLivroNaoExistentePorISBN() throws LivroNaoExistenteException{
 		acervo.consultarPorISBN("54321");
+	}
+	
+	private void cadastrarLivro(String ISBN, String titulo) {
+		Livro livro = new Livro(ISBN, titulo);
+		acervo.cadastrar(livro);
 	}
 }
